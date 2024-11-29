faaa3ea04f3e7d2075852df2fc383f7688717452
Chef Steve Popp <office@memphisfirebbq.com>
2024-11-29 09:05:35 -0500

    Update src/types/index.ts [BoltSync]

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,20 @@
-export * from './auth';
-export * from './organization';
+export interface ExcelData {
+  id: string;
+  itemId: string;
+  productName: string;
+  category: string;
+  subCategory: string;
+  vendor: string;
+  itemCode: string;
+  unitOfMeasure: string;
+  caseSize: string;
+  unitsPerCase: string;
+  price: number;
+  adjustedPrice: number;
+  yieldPercent: number;
+  lastUpdated: string;
+  image?: string;
+}
+
+export * from './recipe';
 export * from './supabase';
