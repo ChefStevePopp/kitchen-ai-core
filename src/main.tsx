4655ffba37f9da3dc32e19c254ed0451d520d46f
Chef Steve Popp <office@memphisfirebbq.com>
2024-11-29 09:07:12 -0500

    Update src/main.tsx [BoltSync]

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import { Toaster } from 'react-hot-toast';
+
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          className: 'card !bg-gray-800 !text-white',
+          duration: 3000,
+        }}
+      />
+    </BrowserRouter>
+  </React.StrictMode>
+);
