444a1592e8c1e2dedfe775ce554b1c2a7cfde663
Chef Steve Popp <office@memphisfirebbq.com>
2024-11-29 09:06:16 -0500

    Update src/features/admin/components/sections/index.ts [BoltSync]

---
+++ src/features/admin/components/sections/index.ts
@@ -0,0 +1,11 @@
+export { AdminDashboard } from '../AdminDashboard';
+export { TeamManagement } from './TeamManagement';
+export { RecipeConfig } from './RecipeConfig';
+export { ExcelImports } from './ExcelImports';
+export { TaskManagement } from './TaskManagement';
+export { NotificationCenter } from './NotificationCenter';
+export { PermissionsManager } from './PermissionsManager';
+export { HelpSupport } from './HelpSupport';
+
+// Import ConfigurationPanel from settings folder
+export { ConfigurationPanel } from '../settings/ConfigurationPanel';
