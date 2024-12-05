59d86d05ae0c1a07dff83ec81f388824dc82e349
Chef Steve Popp <office@memphisfirebbq.com>
2024-12-04 21:20:55 -0500

    Update src/features/allergens/components/index.ts [BoltSync]

--- src/features/allergens/components/index.ts
+++ src/features/allergens/components/index.ts
@@ -1,5 +1,6 @@
 export { AllergenBadge } from './AllergenBadge';
 export { AllergenList } from './AllergenList';
 export { AllergenLegend } from './AllergenLegend';
+export { AllergenSelector } from './AllergenSelector';
 
 export type { AllergenType } from './AllergenBadge';
