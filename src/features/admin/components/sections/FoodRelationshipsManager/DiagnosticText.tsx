c2f81e37f2c9394d9508e0167c0fefd7cff5e336
Chef Steve Popp <office@memphisfirebbq.com>
2024-12-04 21:21:46 -0500

    Update src/features/admin/components/sections/FoodRelationshipsManager/DiagnosticText.tsx [BoltSync]

---
+++ src/features/admin/components/sections/FoodRelationshipsManager/DiagnosticText.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const DiagnosticText: React.FC = () => {
+  return (
+    <div className="text-xs text-gray-500 font-mono mb-4">
+      src/features/admin/components/sections/FoodRelationshipsManager/index.tsx
+    </div>
+  );
+};
