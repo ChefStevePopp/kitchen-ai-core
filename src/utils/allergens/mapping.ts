a34ba5a71e4b439800cac3e261f111cecbd02746
Chef Steve Popp <office@memphisfirebbq.com>
2024-12-04 21:20:48 -0500

    Update src/utils/allergens/mapping.ts [BoltSync]

---
+++ src/utils/allergens/mapping.ts
@@ -0,0 +1,24 @@
+// Map Excel column headers to database column names
+export const ALLERGEN_MAPPING = {
+  'Peanut': 'allergen_peanut',
+  'Crustacean': 'allergen_crustacean',
+  'Tree Nut': 'allergen_treenut',
+  'Shellfish': 'allergen_shellfish',
+  'Sesame': 'allergen_sesame',
+  'Soy': 'allergen_soy',
+  'Fish': 'allergen_fish',
+  'Wheat': 'allergen_wheat',
+  'Milk': 'allergen_milk',
+  'Sulphite': 'allergen_sulphite',
+  'Egg': 'allergen_egg',
+  'Gluten': 'allergen_gluten',
+  'Mustard': 'allergen_mustard',
+  'Celery': 'allergen_celery',
+  'Garlic': 'allergen_garlic',
+  'Onion': 'allergen_onion',
+  'Nitrite': 'allergen_nitrite',
+  'Mushroom': 'allergen_mushroom',
+  'Hot Pepper': 'allergen_hot_pepper',
+  'Citrus': 'allergen_citrus',
+  'Pork': 'allergen_pork'
+} as const;
