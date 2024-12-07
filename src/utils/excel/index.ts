d01bfca7db546013c15d02e25ef200f551ec3343
Chef Steve Popp <office@memphisfirebbq.com>
2024-12-07 14:36:48 -0500

    Update src/utils/excel/index.ts [BoltSync]

--- src/utils/excel/index.ts
+++ src/utils/excel/index.ts
@@ -1,5 +1,3 @@
-export * from './masterIngredients/constants';
-export * from './masterIngredients/validation';
-export * from './masterIngredients/template';
 export * from './foodRelationships';
 export * from './inventory';
+export * from './masterIngredients';
